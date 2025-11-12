979915fdc350c342dcfa5312eb3f632151f5a69c
Vamsim07 <vamsimamidi197@gmail.com>
2025-11-12 22:32:31 +0530

    COUNTER

---
+++ counter/counter.js
@@ -0,0 +1,19 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const counterLabel = document.getElementById("counterLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    counterLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    counterLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0; 
+    counterLabel.textContent = count;
+}
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
+
 let userName;
 document.getElementById("mySubmit").onclick = function(){
     userName=document.getElementById("myText").value;
-    document.getElementById("heading").textContent=(`Hello..! ${userName}`)
+    document.getElementById("heading").textContent=(`Hello..! ${userName}`);
 }
 
 
