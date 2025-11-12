c4ac9c43d91258a1ec1f3b20df206abcde7682ff
Vamsim07 <vamsimamidi197@gmail.com>
2025-11-12 18:36:00 +0530

    cercumference

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById("mySubmit").onclick = function(){
     userName=document.getElementById("myText").value;
     document.getElementById("heading").textContent=(`Hello..! ${userName}`)
 }
+
+
+//const
+
+const PI =3.14159;
+let radius;
+let circumference;
+document.getElementById("myButton").onclick = function(){
+    radius = document.getElementById("myRadius").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("result").textContent= circumference +"cm";
+}
